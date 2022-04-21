0077f55938bcd3d52ef6dbbeaf83a36fcff30816
y-berber <100419372+y-berber@users.noreply.github.com>
2022-04-21 11:08:57 +0300

    Girilen sayıya kadar 2'nin kuvvetlerini yazdırama
    Girilen sayıya kadar olan 2'nin kuvvetlerini ekrana yazdıran program patka.dev ödev

---
+++ java 101/ikiUssuYazdirma.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ikiUssuYazdirma {
+static int i;
+	public static void main(String[] args) {
+		
+	//girilen sayıya kadar 2'nin kuvvetlerini yazdıracağız
+	System.out.println("Lütfen üst sınır sayısını giriniz !");
+	
+	//kullanıcıdan sayımızı alıyoruz
+	Scanner scn=new Scanner(System.in);
+	int input = scn.nextInt();
+	
+	//döngümüzü kuruyoruz ve konsola yazdırıyoruz.
+	for(i=1;i<=input;i*=2){
+		System.out.println(i);
+		scn.close();//veri girişini kapatıyoruz.
+	}
+
+}//main metod kapama
+}//sınıf kapama
