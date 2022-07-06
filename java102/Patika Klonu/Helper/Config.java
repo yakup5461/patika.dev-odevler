1384ebdf241eb5264f8c48d0226f6d20760b259d
yakup <100419372+y-berber@users.noreply.github.com>
2022-07-06 10:44:26 +0300

    Patika Klon Kod Çalışması
    Patika Klon Kod Çalışması patika.dev ödev

---
+++ java102/Patika Klonu/Helper/Config.java
@@ -0,0 +1,8 @@
+
+
+public class Config {
+    public static final String PROJECT_TITLE="Patika.dev";
+    public static final String DB_URL="jdbc:mysql://localhost/patika";
+    public static final String DB_USERNAME = "root";
+    public static final String DB_PASSWORD = "mysql";
+}
---
+++ java102/Patika Klonu/Model/Operator.java
@@ -0,0 +1,9 @@
+public class Operator extends User{
+
+    public Operator() {
+    }
+
+    public Operator(int id, String name, String uname, String pass, String type) {
+        super(id, name, uname, pass, type);
+    }
+}
