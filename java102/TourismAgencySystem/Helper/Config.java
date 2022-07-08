6753fe76bbabcc0a3957969263fbf4ba70afb09e
yakup <100419372+y-berber@users.noreply.github.com>
2022-07-08 11:20:44 +0300

    java-102/odev-turizm-acente projesi
    java-102/odev-turizm-acente projesi patika.dev ödev

---
+++ java102/TourismAgencySystem/Helper/Config.java
@@ -0,0 +1,11 @@
+package TourismAgencySystem.Helper;
+
+public class Config {
+    public static final String PROJECT_TITLE = "Tourism Agency System ";
+    public static final String DB_URL ="jdbc:mysql://localhost:3306/turizm";
+    public static final String DB_USERNAME = "root";
+    public static final String DB_PASSWORD = "";
+
+
+
+}
